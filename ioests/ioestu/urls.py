ce96a7489d02d26b497724e21ee5e49e83352311
basnetbik <basnet.bik@gmail.com>
2013-02-07 14:05:53 +0545

    First project commit by Bikram

---
+++ ioests/ioestu/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('ioestu.views',
+	url(r'^$','index'),
+	url(r'^logged/$','logged'),
+    # Examples:
+    # url(r'^$', 'ioests.views.home', name='home'),
+    # url(r'^ioests/', include('ioests.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+)
