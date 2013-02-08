1441035ac1d193312c7e6f371744621d5a98641e
Pravesh Koirala <praveshkoirala@gmail.com>
2013-02-08 22:31:17 +0545

    Fixed again

--- ioests/ioests/settings.py
+++ ioests/ioests/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ioests',                      # Or path to database file if using sqlite3.
-        'USER': 'pravesh',                      # Not used with sqlite3.
-        'PASSWORD': 'ramhari',                  # Not used with sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': 'bikram',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
