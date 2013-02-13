fe73ee4011928af5a4902397fdf5ba39d7e56d00
basnetbik <basnet.bik@gmail.com>
2013-02-13 12:52:46 +0545

    Bikram

--- ioests/ioestu/endofday.py
+++
@@ -1,6 +0,0 @@
-def backupDatabase():
-	pass
-def notificationTrigger():
-	pass
-def accountingTask():
-	pass
--- ioests/ioestu/views.py
+++ ioests/ioestu/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from ioestu.models import Student,Operator, Activity
-from ioestu.models import Activity
 import datetime
 
 
@@ -14,22 +13,25 @@ def index(request):
     if request.method == 'POST':        
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = str(request.POST.get('user'))
 
         state = "wrong entry"
 
-        if user == 'operator':
-            all_users = Operator.objects.all()
+        try :
+            operator = Operator.objects.get(name=username)
+            if operator.password == password:
+                request.session['data_ioests']={'type':'operator','name':username,'balance_before':'null','action':'payment'}
+                return  HttpResponseRedirect('/logged/') 
 
-        elif user == 'student':
-            all_users = Student.objects.all() 
+        except Operator.DoesNotExist:
+            try:
+                student = Student.objects.get(student_id=username)
+                if student.password == password:
+                    request.session['data_ioests']={'type':'student','name':username,'balance_before':'null','action':'payment'}
+                    return  HttpResponseRedirect('/logged/') 
+            except Student.DoesNotExist:
+                student = None
+                operator = None
 
-        for operator in all_users:
-            if operator.name == username and operator.password == password:
-                del request.session['data_ioests']
-                request.session['data_ioests']={'type':'operator','name':username,'balance_before':'null'}
-                return  HttpResponseRedirect('/logged/')
-        
     return render_to_response('ioestu/index.html',{'state':state, 'username': username},RequestContext(request))
 
 def logged(request):
@@ -64,6 +66,12 @@ def logged(request):
         Activity_latest = Activity.objects.all()
         if Activity_latest:
             Activity_latest = Activity.objects.all().order_by('-date')[0]
-        return render_to_response('ioestu/logged.html',{'state':state,'last_Activity':last_Activity,'Activity_latest':Activity_latest,'balance_before':data_ioests['balance_before']},RequestContext(request))
+        return render_to_response('ioestu/logged_operator.html',{'state':state,'last_Activity':last_Activity,'Activity_latest':Activity_latest,'balance_before':data_ioests['balance_before']},RequestContext(request))
+    
+    else:
+        state = data_ioests['name']+", successfully logged in!"
+        student = Student.objects.get(student_id=data_ioests['name'])
+        activities = Activity.objects.filter(student_id=student.student_id)
+        return render_to_response('ioestu/logged_student.html',{'state':state,'student':student,'activities':activities})
 
 
