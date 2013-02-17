0ce05765de189755bc248b2a78b53f27f26c5c65
Pravesh Koirala <praveshkoirala@gmail.com>
2013-02-17 08:20:35 +0545

    Added new module, validation.py and related functions Added creating new user function Added deposit function Added withdraw function Added closeaccount function

---
+++ ioests/ioestu/validation.py
@@ -0,0 +1,49 @@
+from ioestu.models import Student,Operator, Activity
+import re
+
+#get user with password
+def getstudentp(username,password):
+    try:
+        student = Student.objects.get(student_id=username,password=password)
+        return  student 
+    except Student.DoesNotExist:
+        return None
+    
+#get operator with password
+def getoperatorp(username,password):
+    try :
+        operator = Operator.objects.get(name=username,password=password)
+        return operator
+    except Operator.DoesNotExist:
+        return False
+
+#get student without password
+def getstudent(username):
+    try:
+        student = Student.objects.get(student_id=username)
+        return  student 
+    except Student.DoesNotExist:
+        return None
+
+def getoperator(username):
+    try :
+        operator = Operator.objects.get(name=username)
+        return operator
+    except Operator.DoesNotExist:
+        return False
+
+        
+def usernamevalid(studentid):
+    if re.match(r'^\w+',studentid):
+        return 'True'
+    return r'Student id not matched, the pattern is "[a-bA-B0-9]+"'
+
+def emailvalid(email):
+    if re.match(r'^\w+@\S+\.[a-zA-Z]+',email):
+        return 'True'
+    return r'email not matched, the pattern is "^\w+@\S+\.[a-ZA-Z]+"'
+
+def namevalid(name):
+    if re.match(r'^[a-zA-Z]+$',name):
+        return 'True'
+    return r'name not matched, the pattern is "^[a-zA-Z]+$"'
